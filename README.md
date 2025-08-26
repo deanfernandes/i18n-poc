64976eb059effdaf593cb70f961e20b6177ba1f0
dean <deanfernandes18@gmail.com>
2025-08-26 19:37:43 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# i18n-poc
+
+- i18next
+- translation (en, pt)
+- plural
+- interpolation
+- namespaces
+- nesting
+- context
---
+++ i18next/index.js
@@ -0,0 +1,83 @@
+import i18next from "i18next";
+
+i18next.init(
+  {
+    debug: true,
+    lng: "en",
+    fallbackLng: "en",
+    resources: {
+      en: {
+        translation: {
+          apple_zero: "No apples",
+          apple_one: "An apple",
+          apple_other: "{{count}} Apples",
+          apple: "Apple",
+        },
+        login: {
+          button: {
+            login: "Login",
+          },
+          register: "Need to register an account? Click here",
+          error: "Error logging in ({{error}}). try again later.",
+        },
+        products: {
+          light_weight: "light",
+          light_color: "light",
+        },
+        dashboard: {
+          analytics: {
+            charts: {
+              visits: "Website Visits",
+            },
+          },
+        },
+      },
+      pt: {
+        translation: {
+          apple_zero: "Sem maçãs",
+          apple_one: "Uma maçã",
+          apple_other: "{{count}} Maçãs",
+          apple: "Maçã",
+        },
+        login: {
+          button: {
+            login: "Entrar",
+          },
+          register: "Precisa registrar uma conta? Clique aqui",
+          error: "Erro ao fazer login ({{error}}). Tente novamente mais tarde.",
+        },
+        products: {
+          light_weight: "leve",
+          light_color: "luz",
+        },
+        dashboard: {
+          analytics: {
+            charts: {
+              visits: "Visitas ao site",
+            },
+          },
+        },
+      },
+    },
+  },
+  (err, _) => {
+    if (err) return console.log("error init i18next: ", err);
+  }
+);
+
+console.log(i18next.t("button.login", { ns: "login" }));
+console.log(i18next.t("register", { ns: "login" }));
+console.log(
+  i18next.t("error", { ns: "login", error: "Username already taken" })
+);
+console.log(i18next.t("products:light", { context: "color" }));
+console.log(i18next.t("analytics.charts.visits", { ns: "dashboard" }));
+
+i18next.changeLanguage("pt");
+console.log(i18next.t("login:button.login"));
+console.log(i18next.t("login:register"));
+console.log(i18next.t("login:error", { error: "Username already taken" }));
+console.log(i18next.t("products:light", { context: "color" }));
+console.log(i18next.t("dashboard:analytics.charts.visits"));
+
+console.log(i18next.t("apple", { lng: "de" }));
